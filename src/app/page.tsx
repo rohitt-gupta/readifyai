01acc24aa6f9feec80008560ca2567c1e01ffb17
rohitGupta2022 <pta.rohit28@gmail.com>
2023-09-28 21:15:06 +0530

    add: boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-600">
+      <div>
+        hello
+      </div>
+    </MaxWidthWrapper>
+  )
+}
