c1e0e318b844cfbb3d4d8ba3278417331b8afc5f
rohitGupta2022 <pta.rohit28@gmail.com>
2023-09-28 22:41:54 +0530

    Add: Landing page
    Custom css
    tailwindcss config
    images
    shadcn button

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import clsx from "clsx";
-import { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
