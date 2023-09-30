3c671479235f105684754782c1336b1212ffba36
rohitGupta2022 <pta.rohit28@gmail.com>
2023-09-30 21:12:41 +0530

    Add: Stripe and pricing route

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -18,9 +18,9 @@ export const PLANS = [
     quota: 50,
     pagesPerPdf: 25,
     price: {
-      amount: 14,
+      amount: 2000,
       priceIds: {
-        test: 'price_1NuEwTA19umTXGu8MeS3hN8L',
+        test: 'price_1Nw5G8SEfm1o2VyDt7DTI11m',
         production: '',
       },
     },
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -8,11 +8,11 @@ import { TRPCError } from '@trpc/server'
 import { db } from '@/db'
 import { z } from 'zod'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
-// import { absoluteUrl } from '@/lib/utils'
-// import {
-//   getUserSubscriptionPlan,
-//   stripe,
-// } from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils'
+import {
+  getUserSubscriptionPlan,
+  stripe,
+} from '@/lib/stripe'
 import { PLANS } from '@/config/stripe'
 
 export const appRouter = router({
@@ -105,73 +105,70 @@ export const appRouter = router({
 
       return file
     }),
-  // getUserFiles: privateProcedure.query(async ({ ctx }) => {
-  //   const { userId } = ctx
 
-  //   return await db.file.findMany({
-  //     where: {
-  //       userId,
-  //     },
-  //   })
-  // }),
+  createStripeSession: privateProcedure.mutation(
+    async ({ ctx }) => {
+      const { userId } = ctx
 
-  // createStripeSession: privateProcedure.mutation(
-  //   async ({ ctx }) => {
-  //     const { userId } = ctx
+      const billingUrl = absoluteUrl('/dashboard/billing')
 
-  //     const billingUrl = absoluteUrl('/dashboard/billing')
+      if (!userId)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-  //     if (!userId)
-  //       throw new TRPCError({ code: 'UNAUTHORIZED' })
+      const dbUser = await db.user.findFirst({
+        where: {
+          id: userId,
+        },
+      })
 
-  //     const dbUser = await db.user.findFirst({
-  //       where: {
-  //         id: userId,
-  //       },
-  //     })
+      if (!dbUser)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-  //     if (!dbUser)
-  //       throw new TRPCError({ code: 'UNAUTHORIZED' })
+      const subscriptionPlan =
+        await getUserSubscriptionPlan()
+      //user subscribd to subscription
+      if (
+        subscriptionPlan.isSubscribed &&
+        dbUser.stripeCustomerId
+      ) {
+        const stripeSession =
+          await stripe.billingPortal.sessions.create({
+            customer: dbUser.stripeCustomerId,
+            return_url: billingUrl,
+          })
 
-  //     const subscriptionPlan =
-  //       await getUserSubscriptionPlan()
+        return { url: stripeSession.url }
+      }
+      console.log("user not subscribed");
 
-  //     if (
-  //       subscriptionPlan.isSubscribed &&
-  //       dbUser.stripeCustomerId
-  //     ) {
-  //       const stripeSession =
-  //         await stripe.billingPortal.sessions.create({
-  //           customer: dbUser.stripeCustomerId,
-  //           return_url: billingUrl,
-  //         })
+      // usern not subscribed
+      const stripeSession =
+        await stripe.checkout.sessions.create({
+          success_url: billingUrl,
+          cancel_url: billingUrl,
+          payment_method_types: ['card'],
+          mode: 'subscription',
+          billing_address_collection: 'auto',
+          line_items: [
+            {
+              price: PLANS.find(
+                (plan) => plan.name === 'Pro'
+              )?.price.priceIds.test,
+              quantity: 1,
+            },
+          ],
+          metadata: {
+            userId: userId,
+          },
+        })
+      console.log("stripe session", stripeSession);
 
-  //       return { url: stripeSession.url }
-  //     }
+      console.log("url", stripeSession.url);
 
-  //     const stripeSession =
-  //       await stripe.checkout.sessions.create({
-  //         success_url: billingUrl,
-  //         cancel_url: billingUrl,
-  //         payment_method_types: ['card', 'paypal'],
-  //         mode: 'subscription',
-  //         billing_address_collection: 'auto',
-  //         line_items: [
-  //           {
-  //             price: PLANS.find(
-  //               (plan) => plan.name === 'Pro'
-  //             )?.price.priceIds.test,
-  //             quantity: 1,
-  //           },
-  //         ],
-  //         metadata: {
-  //           userId: userId,
-  //         },
-  //       })
 
-  //     return { url: stripeSession.url }
-  //   }
-  // ),
+      return { url: stripeSession.url }
+    }
+  ),
 
   getFileMessages: privateProcedure
     .input( // since this is a mutation i.e. a POST request it takes in the below type input
