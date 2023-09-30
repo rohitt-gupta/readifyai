a4892de4647766fa7e422b961c105bccffabf07b
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-01 00:03:07 +0530

    Update: Name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,48 @@ export function absoluteUrl(path: string) {
     return `https://${process.env.VERCEL_URL}${path}`
   return `http://localhost:${process.env.PORT ?? 3000
     }${path}`
+}
+
+export function constructMetadata({
+  title = "chatpdf - the SaaS for students",
+  description = "chatpdf is an open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@joshtriedcoding"
+    },
+    icons,
+    metadataBase: new URL('https://chatpdf-jet.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
 }
