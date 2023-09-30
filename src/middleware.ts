ca650e72c348453b6c81e8b42379033e2d570047
rohitGupta2022 <pta.rohit28@gmail.com>
2023-09-30 22:11:16 +0530

    Fix: build error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
+import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export const cofig = {
-  matcher: ["/dashboard/:path*", "auth-callback"]
+export const config = {
+  matcher: ['/dashboard/:path*', '/auth-callback'],
 }
 
 export default authMiddleware
