41c9a202717d7c67b525f1893659cec01a1b7883
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-01 21:30:16 +0530

    fix:get started link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             size: 'lg',
             className: 'mt-5',
           })}
-          href='/dashboard'
+          href='/sign-in'
           target='_blank'>
           Get started{' '}
           <ArrowRight className='ml-2 h-5 w-5' />
