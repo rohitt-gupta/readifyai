2055615164c7d1441eadc1e5da03097e6a3dec5b
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-03 17:19:16 +0530

    Remove: hello

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -188,7 +188,6 @@ const UploadButton = ({
       </DialogTrigger>
 
       <DialogContent>
-        Hello
         <UploadDropzone isSubscribed={isSubscribed} />
       </DialogContent>
     </Dialog>
