15e2abcc1f04c47526200a3f2bda4ef7e8106c86
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-17 12:51:20 +0530

    fix: Prisma generate

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,27 @@
-// import { PrismaClient } from '@prisma/client'
-// import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaClient } from '@prisma/client/edge'
+// // import { PrismaClient } from '@prisma/client'
+// // import { withAccelerate } from '@prisma/extension-accelerate'
+// import { PrismaClient } from '@prisma/client/edge'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient
-}
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var cachedPrisma: PrismaClient
+// }
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-  prisma = global.cachedPrisma
-}
+// let prisma: PrismaClient
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.cachedPrisma) {
+//     global.cachedPrisma = new PrismaClient()
+//   }
+//   prisma = global.cachedPrisma
+// }
 
-export const db = prisma
+// export const db = prisma
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+export const db = prisma;
